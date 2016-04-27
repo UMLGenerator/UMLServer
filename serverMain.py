bf2aa48b9600bea2846a195b33e9f438fad1458b
tmfulton <tfulto@gmail.com>
2016-04-26 21:27:20 -0400

    added zip file support and other new features

--- serverMain.py
+++ serverMain.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import smtplib
+import shutil
 
 from flask import Flask, request, send_file
 from werkzeug import secure_filename
@@ -9,7 +10,10 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
 
-ALLOWED_EXTENSIONS = set(['txt'])
+ALLOWED_EXTENSIONS = set(['txt', 'xmi'])
+
+xmiFilePath = ""
+plantFilePath = ""
 
 # https://www.google.com/settings/security/lesssecureapps
 # Use to allow login from this application
@@ -24,17 +28,21 @@ def hello():
 	return "Add /uml to the end of the address to generate UML!"
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route("/upload", methods=['GET', 'POST'])
 def generateUML():
 	if request.method == 'POST':
-		plantFile = request.files['file']
-		if plantFile and allowed_file(plantFile.filename):
+		plantFile = request.files['plantUML']
+		xmiFile = request.files['xmi']
+		if plantFile and allowed_file(plantFile.filename) and xmiFile and allowed_file(xmiFile.filename):
 			plantFileName = secure_filename(plantFile.filename)
 			plantFile.save(app.config["DIR"] + "/" + plantFileName)
 			plantFilePath = app.config["DIR"] + "/" + plantFileName
+			xmiFileName = secure_filename(xmiFile.filename)
+			xmiFile.save(app.config["DIR"] + "/" + xmiFileName)
+			xmiFilePath = app.config["DIR"] + "/" + xmiFileName
 			plantUMLCommand = ["java", "-jar", "./plantuml.jar", plantFilePath]
 			p = subprocess.call(plantUMLCommand)
 			pngFileName = plantFileName.replace(".txt", ".png")
@@ -44,40 +52,59 @@ def generateUML():
 
 	return "Error? How do I handle this?"
 
+
 # Option in the future
 # Include error checking for if image is there, etc
 @app.route("/email/<user>")
 def sendMailToUser(user):
-  mail(user, "UML Diagram", "Attached is your picture!", imageName)
-  return "Your email is being sent to " + user
+	directory = user + "_UMLDiagram"
+
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+
+	newImageName = plantFilePath.replace('txt', "png")
+	curPathImage = "./" + newImageName
+	newPathImage = directory + "/" + newImageName
+	os.rename(curPathImage, newPathImage)
+
+	curPathXMI = "./" + xmiFilePath
+	newPathXMI = directory + "/" + xmiFilePath
+	os.rename(curPathXMI, newPathXMI)
+
+	zippedFile = directory + ".zip"
+
+	shutil.make_archive(directory,'zip', directory)
+
+	mail(user, "UML Diagram", "Attached is your picture!", zippedFile)
+	return "Your email is being sent to " + user
 
 
 def mail(to, subject, text, attach):
-   msg = MIMEMultipart()
+	msg = MIMEMultipart()
 
-   msg['From'] = gmail_user
-   msg['To'] = to
-   msg['Subject'] = subject
+	msg['From'] = gmail_user
+	msg['To'] = to
+	msg['Subject'] = subject
 
-   msg.attach(MIMEText(text))
+	msg.attach(MIMEText(text))
 
-   part = MIMEBase('application', 'octet-stream')
-   part.set_payload(open(attach, 'rb').read())
-   encoders.encode_base64(part)
-   part.add_header('Content-Disposition',
-           'attachment; filename="%s"' % os.path.basename(attach))
-   msg.attach(part)
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload(open(attach, 'rb').read())
+	encoders.encode_base64(part)
+	part.add_header('Content-Disposition',
+		'attachment; filename="%s"' % os.path.basename(attach))
+	msg.attach(part)
 
-   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
-   mailServer.ehlo()
-   mailServer.starttls()
-   mailServer.ehlo()
-   mailServer.login(gmail_user, gmail_pwd)
-   mailServer.sendmail(gmail_user, to, msg.as_string())
-   # Should be mailServer.quit(), but that crashes...
-   mailServer.close()
+	mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+	mailServer.ehlo()
+	mailServer.starttls()
+	mailServer.ehlo()
+	mailServer.login(gmail_user, gmail_pwd)
+	mailServer.sendmail(gmail_user, to, msg.as_string())
+	# Should be mailServer.quit(), but that crashes...
+	mailServer.close()
 
 
 if __name__ == "__main__" :
 	app.config["DIR"] = os.getcwd()
-	app.run()
+	app.run(debug="True")
